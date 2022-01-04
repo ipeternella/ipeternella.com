aaa2b7fdd17ad940745e2f44d1eb67190bef8767
IgooorGP <igor.feq@gmail.com>
2022-01-04 11:42:58 -0300

    chore(game): add no mobile game disclaimer

--- src/components/Game/index.js
+++ src/components/Game/index.js
@@ -1,10 +1,15 @@
+import useMediaQuery from "@mui/material/useMediaQuery"
 import React, { useEffect } from "react"
 import startGame from "../../game"
 import * as S from "./styled"
 
 const GameComponent = () => {
+  const isDesktop = useMediaQuery("(min-width:720px)") // not a mobile screen (uses react State under the hood)
+
   // Phaser mutates the DOM: it must be executed after this component has been rendered
+  // and the device is not a mobile one
   useEffect(() => {
+    if (!isDesktop) return null
     const gamePromise = startGame(window)
 
     return async () => {
@@ -12,7 +17,16 @@ const GameComponent = () => {
       const game = await gamePromise
       game.destroy()
     }
-  }, [])
+  }, [isDesktop]) // imperative effect re-executed only when isDesktop media query result changes
+
+  if (!isDesktop) {
+    return (
+      <S.NoMobileWrapper>
+        <S.NoMobileText>Sorry, Peter always sleeps on mobile screens... how about trying on a desktop?</S.NoMobileText>
+        <S.PeterSleeping />
+      </S.NoMobileWrapper>
+    )
+  }
 
   return <S.GameWrapper id="game"></S.GameWrapper>
 }
--- src/components/Game/styled.js
+++ src/components/Game/styled.js
@@ -1,8 +1,39 @@
-import { Box } from "@material-ui/core"
+import { Box, Typography } from "@material-ui/core"
 import styled from "styled-components"
+import PeterSleepingGif from "../../../static/gif/peter-sleeping.gif"
 
 export const GameWrapper = styled(Box)`
   margin: 2em auto;
   width: 800px;
   height: 600px;
+`
+
+export const NoMobileWrapper = styled(Box)`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  width: 100%;
+`
+
+export const NoMobileText = styled(Typography).attrs({
+  variant: "h3",
+  align: "center",
+})`
+  font-family: "Fjalla One";
+  margin: 1em 0 0 0;
+  color: ${props => props.theme.palette.text.primary};
+`
+
+export const PeterSleeping = styled.img.attrs({
+  src: PeterSleepingGif,
+})`
+  display: block;
+  width: 400px;
+  margin: 15px 0 0 0;
+  align-self: center;
+
+  /* pixel art */
+  image-rendering: auto;
+  image-rendering: crisp-edges;
+  image-rendering: pixelated;
 `
